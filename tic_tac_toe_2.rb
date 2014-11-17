4efd0d8e676bc16c799e2c5733ae2b294154c10f
Aaron George <aaronwgeorge@gmail.com>
2014-11-17 13:58:18 -0800

    Improved code, added computer AI, added loop to play again

--- tic_tac_toe_2.rb
+++ tic_tac_toe_2.rb
@@ -1,5 +1,6 @@
 require "pry"
 
+#possible ways to win stored in arrays
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 #method to draw gameboard to be filled in by players
@@ -24,18 +25,17 @@ def empty_squares(board)
 end
 
 #check for winner
-def there_is_a_winner(board)
+def there_is_a_winner?(board)
   WINNING_LINES.each do |l|
     if board.values_at(l[0], l[1], l[2]) == ["X", "X", "X"]
       puts "YOU WIN!!"
-      true
+      return true
     elsif board.values_at(l[0], l[1], l[2]) == ["O", "O", "O"]
       puts "YOU LOSE!!"
-      true
-    else
-      false
+      return true
     end
   end
+  false
 end
 
 #check if all squares are filled
@@ -48,11 +48,12 @@ def all_squares_filled?(hash)
   end
 end
 
+#method for player's turn
 def player_turn(board)
   puts "Select a square (from 1-9):"
   choice = gets.chomp.to_i
   
-  until empty_squares(board).each {|i| i == choice} do
+  until empty_squares(board).include?(choice)
     puts "Try again. Squares #{empty_squares(board)} are available:"
     choice = gets.chomp.to_i
   end
@@ -60,39 +61,84 @@ def player_turn(board)
   board[choice] = "X"
 end 
 
-def comp_turn(board)
-
-  #see if comp can attack
-  #if not, see if comp needs to defend
-  #if not, select a random square
-
+#find out if there are two X's or O's in a row so computer makes smart moves
+def two_in_a_row?(hash, marker)
+  if hash.values.count(marker) == 2
+    hash.select {|k, v| v == " "}.keys.first
+  else
+    false
+  end
 end
 
-#create hash to store data for all the squares on the gameboard
-squares = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}
-
-#start game loop
-
-loop do
-  
-  draw_board(squares)
-
-  player_turn(squares)
-
-  draw_board(squares)
-
-  binding.pry
-
-  break if there_is_a_winner(squares) || all_squares_filled?(squares)
+#method for comp's turn
+def comp_turn(board)
 
   puts "Computer thinking..."
   sleep(1.5)
 
-  #computer randomly chooses a square
-  comp_choice = empty_squares(squares).sample
-
-  squares[comp_choice] = "O"
+  attack_square = nil
+  defend_square = nil
+  attacked = nil
+  defended = nil
   
-  break if there_is_a_winner(squares) || all_squares_filled?(squares)
+  #attack if possible
+  WINNING_LINES.each do |l|
+    attack_square = two_in_a_row?({l[0] => board[l[0]], l[1] => board[l[1]], l[2] => board[l[2]]}, "O") 
+    if attack_square
+      board[attack_square] = "O"
+      attacked = true
+      break
+    end
+  end
 
-end 
+  #if cannot attack, then check for defend
+  if !attacked
+    WINNING_LINES.each do |l|
+      defend_square = two_in_a_row?({l[0] => board[l[0]], l[1] => board[l[1]], l[2] => board[l[2]]}, "X")
+      if defend_square 
+        board[defend_square] = "O"
+        defended = true
+        break
+      end
+    end
+  end
+
+  #if can't defend, select a random square
+  if !attacked && !defended
+    comp_choice = empty_squares(board).sample
+    board[comp_choice] = "O"
+  end
+
+end    
+
+#start game loop
+
+play_again = "Y"
+
+while play_again == "Y" do
+  
+  #create hash to store data for all the squares on the gameboard
+  squares = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}
+  
+  loop do
+    
+    draw_board(squares)
+    
+    player_turn(squares)
+
+    draw_board(squares)
+
+    break if there_is_a_winner?(squares) || all_squares_filled?(squares)
+
+    comp_turn(squares)
+
+    draw_board(squares)
+  
+    break if there_is_a_winner?(squares) || all_squares_filled?(squares)
+
+  end
+
+  puts "Play again? (Y/N)"
+  play_again = gets.chomp.upcase
+
+end
